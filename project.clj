ff7a4345bc9a4979daa7ee548922d87da5c143f5
Mike Fikes <mike@fikesfarm.com>
2015-05-03 21:18:10 -0400

    Initial project structure

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject bocko "0.1.0-SNAPSHOT"
+  :description "Simple imperative graphics"
+  :url "https://github.com/mfikes/bocko"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
