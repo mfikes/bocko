380744d4e6d917d9abbd93cd8d17b81345ba6e45
Mike Fikes <mike@fikesfarm.com>
2015-05-06 14:39:34 -0400

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bocko "0.1.0"
+(defproject bocko "0.2.0"
   :description "Simple imperative graphics"
   :url "https://github.com/mfikes/bocko"
   :license {:name "Eclipse Public License"
