4841efd573cb7800248d67bb8e755a9965a300e1
Mike Fikes <mike@fikesfarm.com>
2015-05-15 09:18:22 -0400

    Update to Clojure 1.7.0-beta3

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/mfikes/bocko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]])
